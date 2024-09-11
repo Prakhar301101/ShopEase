a198fd44627f5a6a6fa4a1bddb0659772b0f42f1
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-09-11 20:22:32 +0530

    Add,Display Category contollers

--- services/product-service/controllers/prodController.js
+++ services/product-service/controllers/prodController.js
@@ -1,54 +1,120 @@
-const product=require('../models/product');
-const category=require('../models/category');
+const Product = require('../models/product');
+const Category = require('../models/category');
 
 // @desc    Display All products
 // @route   GET /api/product/view
 // @access  Public
 
-module.exports.displayAll= async (req,res)=>{
+module.exports.displayAll = async (req, res) => {
+  try {
+    const products = await Product.find();
+    if (products.length > 0) {
+      console.log(products);
+      return res.status(200).json({
+        message: 'Success',
+      });
+    } else {
+      return res.status(400).json({
+        message: 'No Products to display',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: 'Error fetching request, Try again later',
+    });
+  }
+};
 
-}
+// @desc    View all Categories
+// @route   GET /api/product/categories
+// @access  Public
 
+module.exports.displayCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    if (categories.length > 0) {
+      return res.status(200).json({
+        categories
+      });
+    } else {
+      return res.status(400).json({
+        message: 'No Categories to display',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: 'Error fetching request, Try again later',
+    });
+  }
+};
 
-// @desc    Display with Criterias
+// @desc    Display Products with Criterias
 // @route   POST /api/product/view?q
 // @access  Public
 
-module.exports.displaySelected= async (req,res)=>{
-
-}
-
-
+module.exports.displaySelected = async (req, res) => {};
 
 // @desc    Insert a new Category
 // @route   POST /api/product/addCategory
 // @access  Private
 
-module.exports.addCategory= async (req,res)=>{
-
-}
-
+module.exports.addCategory = async (req, res) => {
+  const Role = req.user.role;
+  try {
+    if (Role === 'admin') {
+      const { name, description } = req.body;
+      if (!name || !description) {
+        return res.status(400).json({
+          message: 'Provide all details',
+        });
+      } else {
+        const doc =await Category.findOne({ name });
+        if (doc) {
+            return res.status(400).json({
+                message: 'Category already Exists',
+              });
+        } else {
+            const category_Doc=await Category.create({name,description});
+            if(category_Doc){
+                return res.status(200).json({
+                    message:'Creation Successful',
+                    id:category_Doc._id
+                })
+            }
+            else return res.status(400).json({
+                message:'Creation Failed!'
+            })
+        }
+      }
+    } else {
+      return res.status(401).json({
+        message: 'Not Authorised!',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: 'Error fetching request, Try again later',
+    });
+  }
+};
 
 // @desc    Insert a new product
 // @route   POST /api/product/addProduct
 // @access  Private
 
-module.exports.addProduct= async (req,res)=>{
-
-}
+module.exports.addProduct = async (req, res) => {};
 
 // @desc    Update a product by ID
 // @route   POST /api/product/update/:id
 // @access  Private
 
-module.exports.updateDetails= async (req,res)=>{
-
-}
+module.exports.updateDetails = async (req, res) => {};
 
 // @desc    Delete a product by ID
 // @route   POST /api/product/delete/:id
 // @access  Private
 
-module.exports.remove= async (req,res)=>{
-
-}
+module.exports.remove = async (req, res) => {};
--- services/product-service/routes/productRoutes.js
+++ services/product-service/routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express =require('express');
 const router=express.Router();
 const authorisation=require('../middlewares/authorisation');
-const { addProduct,addCategory,updateDetails,remove,displayAll,displaySelected } = require('../controllers/prodController');
+const { addProduct,addCategory,updateDetails,remove,displayAll,displaySelected,displayCategories } = require('../controllers/prodController');
 
 
-router.get('/get',displayAll);
+router.get('/all',displayAll);
+router.get('/categories',displayCategories);
 router.get('/get/:id',displaySelected);
 router.post('/addProduct',authorisation,addProduct);
 router.post('/addCategory',authorisation,addCategory);
