5bc60d7930f3c9f90fdb3c0c01bc9dca54004cbf
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-09-18 22:14:40 +0530

    Order service setup and model

--- services/order-service/index.js
+++ services/order-service/index.js
@@ -1,13 +1,37 @@
+require('dotenv').config();
 const express=require('express');
 const app=express();
+const PORT=process.env.PORT;
+const cors=require('cors');
+const cookieParser=require('cookie-parser');
+const mongoose=require('mongoose');
 
-app.get('/',(req,res)=>{
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:'*',
+    credentials:true
+}));
+
+
+app.get('/info',(req,res)=>{
     res.json({
         message:"We are at the order service",
-        port:5004
+        PORT
     })
 })
 
-app.listen(5004,()=>{
-    console.log(`Listening at PORT 5004`);
+app.listen(PORT,()=>{
+    console.log(`Listening at PORT ${PORT}`);
 });
+
+
+/**
+ * Order routes
+ * POST /  create an order by calling cart
+ * then it will make a reqeust to payment service and   * update the order detail accordingly
+ * GET /id show order by id
+ * GET / show all your orders
+ * 
+ * 
+ */
---
+++ services/order-service/models/order.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+      products: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      totalPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      address:{
+        type:String,
+        required:true
+      },
+      paymentStatus:{
+        type:String,
+        enum:[pending,successful,failed],
+        required:true
+      }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = model('User', userSchema);
+module.exports = userModel;
